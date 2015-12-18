ae1b0bd7c5ad3c4434e3682cd656fe83f62f74f9
iawls <falken1994@hotmail.com>
2015-12-18 18:54:41 +0100

    Fixed crash at ship death when playing against AI
    Added s.setStartEnd(start, end) to Board.placeShip()

--- GameEngine.cs
+++ GameEngine.cs
@@ -384,7 +384,7 @@ namespace Battleships
         static void Main()
         {
             Board p1 = new Board(true); //Player 1 is human
-            Board p2 = new Board(true); //PLayer 2 is AI
+            Board p2 = new Board(false); //PLayer 2 is AI
             GameEngine GE = new GameEngine(p1, p2);
             GameScreen gameScreen = new GameScreen(GE, p1, p2);
             gameScreen.ShowDialog();     
@@ -471,6 +471,7 @@ namespace Battleships
                   for (int x = start.Item2; x <= end.Item2; x++)
                   {
                       GameBoard.ElementAt(x).ElementAt(y).setShip(s);
+                      s.setStartEnd(start, end);
                   }
                }
               printBoard();
